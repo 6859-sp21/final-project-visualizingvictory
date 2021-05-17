3c4117c79755ce4b93c1b2081c958b02af79ac32
mletoile <letoilemax@gmail.com>
2021-05-16 21:47:47 -0400

    final?
    Near final version

--- scroller.js
+++ scroller.js
@@ -6,6 +6,16 @@ var figure = scrolly.select("figure");
 var article = scrolly.select("article");
 var step = article.selectAll(".step");
 
+let figureCaptions = ["The Liberty Ship John Brown",
+						"The Sinking of the Arizona at Pearl Harbor",
+						"Harriet Tubman and Nathan Bedford Forrest, the first grand wizard of the KKK, both had liberty ships bearing their name",
+						"Rosie would have been a welder if she'd built liberty ships",
+						"The Schenectady was one of several Liberty ships to experience catastrophic cracking",
+						"Constance Tipper solved the mystery of low-temperature embrittlement in steels used on Liberty Ships",
+						"The Red Oak Victory is the only surviving Victory Ship",
+						"Aside from helping build the Hoover Damn and over 1,000 ships, Henry Kaiser also started Kaiser Permanente to provide healthcare for his workers and their families",
+						"American service members in Paris, celebrating the end of the war"]
+
 // initialize the scrollama
 var scroller = scrollama();
 
@@ -45,7 +55,9 @@ function handleStepEnter(response) {
 		.transition()
 		.style("opacity",1);
 
-	console.log(d3.selectAll(`#scroller-image${response.index+1}`));
+	document.getElementById("main-caption").innerHTML =	`Figure ${response.index+1}: ${figureCaptions[response.index]}`;
+	console.log(document.getElementById("main-caption"));
+	console.log("hello");
 }
 
 function setupStickyfill() {
